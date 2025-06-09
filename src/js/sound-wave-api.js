ce741738697e3bb407276a14bd8f697c69912915
Roman-Isko <roman_isko@ukr.net>
2025-06-09 15:03:01 +0300

    modified

--- src/js/sound-wave-api.js
+++ src/js/sound-wave-api.js
@@ -1,16 +1,45 @@
 // alert('api')
+// import axios from 'axios';
+
+// const BASE_URL = 'https://sound-wave.b.goit.study/api';
+
+// async function getArtists() {
+//   try {
+//     const response = await axios.get(`${BASE_URL}/artists`);
+//     console.log('Список артистів:', response.data);
+//   } catch (error) {
+//     console.error('Помилка при отриманні артистів:', error);
+//   }
+// }
+
+// getArtists();
+
 import axios from 'axios';
-
-const BASE_URL = 'https://sound-wave.b.goit.study/api';
-
-async function getArtists() {
+const soundWaveApi = axios.create({
+  baseURL: 'https://sound-wave.b.goit.study/api',
+});
+export async function getArtists() {
   try {
-    const response = await axios.get(`${BASE_URL}/artists`);
-    console.log('Список артистів:', response.data);
+    return await soundWaveApi.get('/artists');
   } catch (error) {
-    console.error('Помилка при отриманні артистів:', error);
+    console.error('Помилка при отриманні списку артистів:', error);
+    throw error;
   }
 }
-
-getArtists();
+export async function getArtistByID(id) {
+  try {
+    return await soundWaveApi.get(`/artists/${id}`);
+  } catch (error) {
+    console.error(`Помилка при отриманні актора з ID ${id}:`, error);
+    throw error;
+  }
+}
+export async function getFeedbacks() {
+  try {
+    return await soundWaveApi.get('/feedbacks');
+  } catch (error) {
+    console.error('Помилка при отриманні відгуків:', error);
+    throw error;
+  }
+}
 
