738d763c9a91b4090651606aef375d00ca393616
uty <fgmapam@gmail.com>
2025-06-09 17:24:52 +0300

    summary(required)

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import './js/render-functions.js'
 import './js/menu.js'
 import './js/modal.js'
 
-import './js/footer.js'
+
 import { createFooter } from "./js/footer.js";
 
-// ...
+const root = document.getElementById("root");
+if (!root) {
+  console.error("❌ Елемент #root не знайдено у index.html");
+  // альтернатива: const root = document.body;
+}
+
 root.append(createFooter());
