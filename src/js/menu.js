caba8955eb87f60a1ed5536cd3dfb979f8ad8cde
OlehHnatenko <sinatorgnatenko93@gmail.com>
2025-06-09 17:42:17 +0300

    template

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,1 +1,37 @@
 // alert('menu')
+
+// const menuBtn = document.querySelector('.menu-btn');
+// const menuList = document.querySelector('.menu-list');
+// const body = document.body;
+// const svgBurger = document.querySelector('.burger-svg');
+
+// menuBtn.addEventListener('click', openMenu);
+// menuList.addEventListener('click', (evt) => {
+    
+
+//     if(evt.target.nodeName === 'A') {
+        
+//         menuList.classList.remove('active');
+//         svgBurger.setAttribute('src', './svg/burger.svg');
+//         body.classList.remove('lock');
+
+//     }
+//     return;
+// });
+
+
+// function openMenu(){
+    
+//     menuList.classList.toggle('active');
+
+//     if(menuList.classList.contains('active')){
+//         svgBurger.setAttribute('src', './svg/close.svg');
+//         body.classList.add('lock');
+//     }else{
+//         svgBurger.setAttribute('src', './svg/burger.svg');
+//         body.classList.remove('lock');
+//     }
+
+//     return
+
+// }
