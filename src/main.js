d24a6966a7f42bcc9725dab40d385121d3331734
uty <fgmapam@gmail.com>
2025-06-09 17:34:58 +0300

    summary(required)

--- src/main.js
+++ src/main.js
@@ -4,12 +4,3 @@ import './js/menu.js'
 import './js/modal.js'
 
 
-import { createFooter } from "./js/footer.js";
-
-const root = document.getElementById("root");
-if (!root) {
-  console.error("❌ Елемент #root не знайдено у index.html");
-  // альтернатива: const root = document.body;
-}
-
-root.append(createFooter());
