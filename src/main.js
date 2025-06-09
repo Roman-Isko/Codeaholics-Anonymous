6dfab31ebb07b24c2575ea928bf7d4d3572bfc89
uty <fgmapam@gmail.com>
2025-06-09 16:49:25 +0300

    summary(required)

--- src/main.js
+++ src/main.js
@@ -2,3 +2,7 @@ import './js/sound-wave-api.js'
 import './js/render-functions.js'
 import './js/menu.js'
 import './js/modal.js'
+
+import { createFooter } from "./js/footer.js";
+// ...
+root.append(createFooter());
