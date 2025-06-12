d993298864f4a24afcdb621ed098314208da88ce
Roman-Isko <roman_isko@ukr.net>
2025-06-13 00:06:34 +0300

    modified:   src/partials/about.html

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,6 +1,6 @@
 
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
 
 
 import 'css-star-rating/css/star-rating.min.css';
